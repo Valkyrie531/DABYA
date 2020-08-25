4f25fc078e80d4878ed0f8867e1f72bcb63f2cb7
Annie Ng <anniepageng@gmail.com>
2020-08-25 12:48:41 +1200

    main menu creation

---
+++ DABYA/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void openLevelSelect()
+    {
+        Debug.Log("Open level select");
+    }
+
+
+    public void openCredits()
+    {
+        Debug.Log("Open credits");
+    }
+
+    public void closeGame()
+    {
+        Debug.Log("close game");
+        Application.Quit();
+    }
+}
