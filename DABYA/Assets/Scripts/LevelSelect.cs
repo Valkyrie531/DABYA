0b2450339c754bf0877b02c3d14f4883754178cb
Annie Ng <anniepageng@gmail.com>
2020-08-27 16:37:40 +1200

    added commenting

--- DABYA/Assets/Scripts/LevelSelect.cs
+++ DABYA/Assets/Scripts/LevelSelect.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
+    //changes the scene from the level selection screen to the main menu screen
     public void returnToMainMeun()
     {
         SceneManager.LoadScene("MainMenu");
--- DABYA/Assets/Scripts/MainMenu.cs
+++ DABYA/Assets/Scripts/MainMenu.cs
@@ -3,18 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    //Changes the scene from the main menu to the level selection screen
     public void openLevelSelect()
     {
         SceneManager.LoadScene("LevelSelect");
     }
 
-
-    public void openCredits()
-    {
-        SceneManager.LoadScene("Credits");
-    }
-
+    //completely closes the game
     public void closeGame()
     {
         Application.Quit();
