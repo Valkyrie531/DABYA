317a7f7d4e70518c4285e17058a821fb35d5bb85
bnort <brett.d.norton@gmail.com>
2020-08-27 18:52:29 +1200

    monster creation
    branch for ongoing monster development

--- DABYA/Assets/Scripts/Monster.cs
+++ DABYA/Assets/Scripts/Monster.cs
@@ -1,13 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Monster : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float startSpeed = 10f;
+
+    [HideInInspector]
+    public float speed;
+
+    public float startHealth = 100;
+    private float health;
+
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
+    private bool isDead = false;
+
     void Start()
     {
-        
+        speed = startSpeed;
+        health = startHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
+
+        if (health <= 0 && !isDead)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        isDead = true;
+
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
---
+++ DABYA/Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Monster))]
+public class MonsterMovement : MonoBehaviour
+{
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private Monster monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monster = GetComponent<Monster>();
+
+        target = Waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * monster.speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+
+        monster.speed = monster.startSpeed;
+    }
+
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            EndPath();
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+
+    void EndPath()
+    {
+        Destroy(gameObject);
+    }
+}
