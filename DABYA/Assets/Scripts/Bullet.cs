8ccbe5c702c1987c62f92d42d0ce05229cf3dc0a
yyf9511 <66350676+yyf9511@users.noreply.github.com>
2020-09-10 14:19:15 +1200

    Setup Tower and Bullet

---
+++ DABYA/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+
+    private Transform bullet;
+    private Vector2 bulletMovement;
+    private GameObject enemy;
+
+
+    void Start()
+    {
+        bullet = GameObject.FindGameObjectWithTag("Enemy").transform;
+        bulletMovement = new Vector2(bullet.position.x, bullet.position.y);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, bullet.position, speed * Time.deltaTime);
+    }
+}
--- DABYA/Assets/Scripts/Tower.cs
+++ DABYA/Assets/Scripts/Tower.cs
@@ -2,17 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Script for Tower setting to kill the monster by tracking as the Target. 
+ * 
+ */
 public class Tower : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform target;
+
+    [Header("Tower Setting")]
+    public float range = 5f;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    [Header("Unity Stuff")]
+    public string enemyTag = "Enemy";
+    public GameObject bullet;
+
+    /* start game with updating target after each 0.1s.
+     * 
+     */
+
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.1f);
+
+        timeBtwShots = startTimeBtwShots;
     }
 
-    // Update is called once per frame
+    /* locate the nearest monster as the target.
+    *  When muliple monsters come into the tower range , aim the nearest monster.
+    */
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        //looping each monster as tag enemy for distance.
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+        
+
+    }
+
+
+
+    /* 
+    *  
+    */
     void Update()
     {
-        
+        if (target == null)
+        {
+            return;
+        }
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+  
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    } 
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
