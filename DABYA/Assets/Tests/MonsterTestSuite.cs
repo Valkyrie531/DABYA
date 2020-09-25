7df1abe881d0c1b0f061adb87485f3d5b7415790
bnort <brett.d.norton@gmail.com>
2020-09-25 15:29:02 +1200

    monster tests
    monster test suite actually commits this time hopefully?

---
+++ DABYA/Assets/Tests/MonsterTestSuite.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using NUnit.Framework.Interfaces;
+
+public class MonsterTestSuite
+{
+    private LevelManager levelManager;
+
+    [SetUp]
+    public void SetUp()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    [UnityTest]
+    public IEnumerator MonsterSpawns()
+    {
+        levelManager = Transform.FindObjectOfType<LevelManager>();
+
+        levelManager.levelSpawner.closeUpgradeMenu();
+
+        yield return new WaitForSeconds(0.1f);
+
+        levelManager.levelSpawner.spawnMonster.InstantiateBasic();
+
+        Assert.NotZero(levelManager.levelSpawner.spawnMonster.monstersSpawned.Count);
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator MonstersCleared()
+    {
+        levelManager = Transform.FindObjectOfType<LevelManager>();
+
+        levelManager.levelSpawner.closeUpgradeMenu();
+
+        yield return new WaitForSeconds(0.1f);
+
+        levelManager.levelSpawner.spawnMonster.InstantiateBasic();
+
+        levelManager.levelSpawner.spawnMonster.clearMonsters();
+
+        Assert.Zero(levelManager.levelSpawner.spawnMonster.monstersSpawned.Count);
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator MonstersMoveAutomatically()
+    {
+        levelManager = Transform.FindObjectOfType<LevelManager>();
+
+        levelManager.levelSpawner.closeUpgradeMenu();
+
+        yield return new WaitForSeconds(0.1f);
+
+        levelManager.levelSpawner.spawnMonster.InstantiateBasic();
+
+        float initialXPos = levelManager.levelSpawner.spawnMonster.monstersSpawned[0].transform.position.x;
+
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.Greater(levelManager.levelSpawner.spawnMonster.monstersSpawned[0].transform.position.x, initialXPos);
+        yield return null;
+    }
+}
