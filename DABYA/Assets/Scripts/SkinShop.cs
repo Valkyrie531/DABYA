8e78435843a6a29bd3123f8340b5f3e14babb307
Rangatron <42063246+Rangatron@users.noreply.github.com>
2020-10-24 16:29:54 +1300

    made skin shop scene and made design
    made the SkinShop scene and made buttons to swap between the panels that will hold the skins for each monster

---
+++ DABYA/Assets/Scripts/SkinShop.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkinShop : MonoBehaviour
+{
+    public GameObject defaultMonPanel;
+    public GameObject speedMonPanel;
+    public GameObject tankMonPanel;
+    public Player user;
+    private int skinPrice = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OpenDefaultSkinPanel()
+    {
+        CloseSpeedSkinPanel();
+        CloseTankSkinPanel();
+        defaultMonPanel.SetActive(true);
+    }
+
+    public void CloseDefaultSkinPanel()
+    {
+        defaultMonPanel.SetActive(false);
+    }
+
+    public void OpenSpeedSkinPanel()
+    {
+        CloseTankSkinPanel();
+        CloseDefaultSkinPanel();
+        speedMonPanel.SetActive(true);
+    }
+
+    public void CloseSpeedSkinPanel()
+    {
+        speedMonPanel.SetActive(false);
+    }
+
+    public void OpenTankSkinPanel()
+    {
+        CloseDefaultSkinPanel();
+        CloseSpeedSkinPanel();
+        tankMonPanel.SetActive(true);
+    }
+
+    public void CloseTankSkinPanel()
+    {
+        tankMonPanel.SetActive(false);
+    }
+}
